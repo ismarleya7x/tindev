ad5cf005348325e2b19d7284a5a50eb798e8cd39
IsmarleyA7X <ismarley_2005@msn.com>
2019-08-09 17:35:52 -0300

    Commit Final

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -5,6 +5,8 @@ module.exports = {
         //console.log(req.params.devId);
         //console.log(req.headers.user);
 
+        console.log(req.io, req.connectedUsers);
+
         const { devId } = req.params;
         const { user } = req.headers;
 
@@ -16,7 +18,16 @@ module.exports = {
         }
 
         if (targetDev.likes.includes(loggedDev._id)) {
-            console.log("Deu match");
+            const loggedSocket = req.connectedUsers[user];
+            const targetSocket = req.connectedUsers[devId];
+
+            if(loggedSocket){
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if(targetSocket){
+                req.io.to(targetSocket).emit('match', loggedDev);
+            }
         }
 
         loggedDev.likes.push(targetDev._id);
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,34 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+
+    connectedUsers[user] = socket.id;
+});
+
 
 mongoose.connect('mongodb+srv://root:a123b321@cluster0-bbhlj.mongodb.net/omnistack?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
-server.use(cors());
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
 
-server.use(express.json());
+    return next();
+});
 
-server.use(routes);
+app.use(cors());
+
+app.use(express.json());
+
+app.use(routes);
 
 server.listen(3333);
